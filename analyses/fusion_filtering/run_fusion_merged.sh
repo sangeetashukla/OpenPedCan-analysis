c33db723de49a4e951c6ad84e7926fb9720bcc49
runjin326 <47674661+runjin326@users.noreply.github.com>
2021-10-06 16:55:15 -0400

    rerun with v10

--- analyses/fusion_filtering/run_fusion_merged.sh
+++ analyses/fusion_filtering/run_fusion_merged.sh
@@ -20,7 +20,7 @@ script_directory="$(perl -e 'use File::Basename;
 cd "$script_directory" || exit
 
 # Set up paths to data files consumed by analysis, and path to result output
-data_path="../../data"
+data_path="../../data/v10"
 scratch_path="../../scratch"
 references_path="references"
 results_path="results/"
@@ -45,18 +45,17 @@ rna_expression_file="${data_path}/gene-expression-rsem-tpm-collapsed.rds"
 normal_expression_adrenal_gland="${references_path}/gtex_adrenal_gland_TPM_hg38.rds"
 normal_expression_brain="${references_path}/gtex_brain_TPM_hg38.rds"
 
+# independent sample list
+independent_RNA_primary="../independent-samples/results/independent-specimens.rnaseq.primary.tsv"
+independent_RNA_relapse="../independent-samples/results/independent-specimens.rnaseq.relapse.tsv"
+   
 # metadata files
 if [[ RUN_FOR_SUBTYPING -eq "0" ]]
 then
    histologies_file="${data_path}/histologies.tsv" 
-   independent_RNA_primary="../independent-samples/results/independent-specimens.rnaseq.primary.tsv"
-   independent_RNA_relapse="../independent-samples/results/independent-specimens.rnaseq.relapse.tsv"
 else 
    histologies_file="${data_path}/histologies-base.tsv"  
-   independent_RNA_primary="../independent-samples/results/independent-specimens.rnaseq.primary.tsv"
-   independent_RNA_relapse="../independent-samples/results/independent-specimens.rnaseq.relapse.tsv"
 fi
-
 
 # data release files to use for recurrent fusion/fused genes detection
 
