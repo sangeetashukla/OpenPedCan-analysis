3d781b35803423eb5c4ee0178e2efd7bbaf3778c
komalsrathi <komal.rathi@gmail.com>
2022-04-27 17:00:47 -0400

    update EPN subtyping

---
+++ analyses/molecular-subtyping-EPN/01-assign-disease-group.R
@@ -0,0 +1,57 @@
+# Author: Komal S. Rathi
+# R version of 01-make_notebook_RNAandDNA.py (Author: Teja Koganti)
+# script to map DNA and RNA samples to a participant and assign disease group
+
+# load libraries
+suppressPackageStartupMessages({
+  library(dplyr)
+  library(optparse)
+})
+
+# Parse command line options
+option_list <- list(
+  make_option(c("--histology"), type = "character",
+    help = "histology file tsv"),
+  make_option(c("--outfile"), type = "character",
+    help = "output tsv file; .gz for gzipped output.")
+)
+opt <- parse_args(OptionParser(option_list = option_list))
+pbta_histologies <- read.delim(opt$histology)
+outfile <- opt$outfile
+
+# infra and supra categories
+infra = c("posterior fossa", "optic", "spinal", "tectum", "spine")
+supra = c("frontal lobe", "parietal lobe", "occipital lobe", "temporal lobe")
+
+# filter for ependymoma samples 
+EP = pbta_histologies %>%
+  filter(pathology_diagnosis =="Ependymoma")
+
+# filter for RNA samples
+EP_rnaseq_samples = EP %>%
+  filter(experimental_strategy == "RNA-Seq") %>%
+  dplyr::select(Kids_First_Biospecimen_ID, Kids_First_Participant_ID, sample_id, primary_site) %>%
+  dplyr::rename("Kids_First_Biospecimen_ID_RNA" = "Kids_First_Biospecimen_ID")
+
+# filter for DNA samples 
+WGS_dnaseqsamples = EP %>%
+  filter(experimental_strategy == "WGS") %>%
+  dplyr::select(Kids_First_Biospecimen_ID, Kids_First_Participant_ID, sample_id, primary_site) %>%
+  dplyr::rename("Kids_First_Biospecimen_ID_DNA" = "Kids_First_Biospecimen_ID")
+
+# sample_id is common between both  datafarmes and also unique between RNA and DNA. 
+# Some DNA BSID's are missing for the corresponding RNA samples
+EP_rnaseq_WGS = EP_rnaseq_samples %>%
+  full_join(WGS_dnaseqsamples, by = c("sample_id", "Kids_First_Participant_ID","primary_site"))
+
+# add disease group (supra/infra category) inferred from primary_site
+EP_rnaseq_WGS <- EP_rnaseq_WGS %>%
+  mutate(disease_group = ifelse(grepl(paste0(supra, collapse = "|"), tolower(primary_site)), "supratentorial", 
+                                ifelse(grepl(paste0(infra, collapse = "|"), tolower(primary_site)), "infratentorial", "undetermined"))) %>%
+  arrange(Kids_First_Participant_ID, sample_id) %>%
+  dplyr::select(Kids_First_Participant_ID, sample_id, 
+                Kids_First_Biospecimen_ID_DNA, Kids_First_Biospecimen_ID_RNA,
+                disease_group)
+
+# write out
+readr::write_tsv(EP_rnaseq_WGS, outfile)
