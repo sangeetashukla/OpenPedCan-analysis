63c597a3876cc2311ed45d86b3fdcaabe1e11a19
jharenza <jolynnerokita@d3b.center>
2022-07-07 20:11:49 -0400

    revert script 04 for pre-release

--- analyses/independent-samples/04-generate-independent-specimens-rnaseq-pre-release.R
+++ analyses/independent-samples/04-generate-independent-specimens-rnaseq-pre-release.R
@@ -1,7 +1,6 @@
 # load libraries
 library(magrittr)
 library(dplyr)
-library(readr)
 
 # base directories
 root_dir <- rprojroot::find_root(rprojroot::has_dir(".git"))
@@ -9,9 +8,8 @@ analysis_dir <- file.path(root_dir, "analyses", "independent-samples")
 out_dir <- file.path(analysis_dir, "results")
 dir.create(out_dir, showWarnings = F, recursive = T)
 
-# source functions
-source(file.path(analysis_dir, "util", "independent-dna-samples.R"))
-source(file.path(analysis_dir, "util", "independent-rna-samples.R"))
+# source function
+source(file.path(analysis_dir, "util", "independent-samples.R"))
 
 
 # read histology file
@@ -21,69 +19,32 @@ histology_df <- readr::read_tsv(file.path(root_dir, 'data/histologies-base.tsv')
 set.seed(100)
 histology_df <- histology_df[sample(nrow(histology_df)), ]
 
-# Filter for dna tumors samples, where composition is not "Derived Cell Line".
-dna_samples <- histology_df %>%
+# Filter to only samples from tumors from RNA-Seq minus GTEX
+# Note that there are some samples with unknown composition, but these will be ignored for now.
+rnaseq_samples <- histology_df %>%
   dplyr::filter(sample_type == "Tumor", 
                 composition != "Derived Cell Line", 
-                is.na(RNA_library), 
-                experimental_strategy %in% c("WGS", "WXS", "Targeted Sequencing"))
+                experimental_strategy == "RNA-Seq")
 
-# filter for dna wgs samples
-wgs_primary_plus_all <- dna_samples %>% 
-  dplyr::filter(experimental_strategy == "WGS") %>% 
-  independent_dna_samples(tumor_types = "prefer_primary", 
-                          independent_level = "all-cohorts-pre-release", seed = 2020)
+print(nrow(rnaseq_samples))
+# generate release RNA-Seq independent samples for all cohorts
+rnaseq_primary_all <- independent_samples(rnaseq_samples, tumor_types = "primary", independent_level = "all-cohorts-release", seed = 2020)
+rnaseq_relapse_all <- independent_samples(rnaseq_samples, tumor_types = "relapse", independent_level = "all-cohorts-release", seed = 2020)
+rnaseq_primary_plus_all <- independent_samples(rnaseq_samples, tumor_types = "prefer_primary", independent_level = "all-cohorts-release", seed = 2020)
 
-# filter for dna wxs samples
-wxs_primary_plus_all <- dna_samples %>% 
-  dplyr::filter(experimental_strategy == "WXS") %>% 
-  independent_dna_samples(tumor_types = "prefer_primary", 
-                          independent_level = "all-cohorts-pre-release", seed = 2020)
 
-# filter for dna panel samples
-panel_primary_plus_all <- dna_samples %>% 
-  dplyr::filter(experimental_strategy == "Targeted Sequencing") %>% 
-  independent_dna_samples(tumor_types = "prefer_primary", 
-                          independent_level = "all-cohorts-pre-release", seed = 2020)
-# dna independent samples 
-independent_dna_sample_df_all <- rbind(wgs_primary_plus_all, wxs_primary_plus_all, panel_primary_plus_all) %>%
-  distinct(Kids_First_Participant_ID, .keep_all = TRUE) %>% 
-  dplyr::arrange(Kids_First_Biospecimen_ID)
-
-# Filter for tumor samples where composition is not Derived Cell Line
-rnaseq_samples <- histology_df %>%
-  dplyr::filter(sample_type == "Tumor", composition != "Derived Cell Line")
-
-# write independent sample outputs for independent levels of all cohorts 
-rnaseq_primary_all_file <- file.path(out_dir, "independent-specimens.rnaseqpanel.primary.pre-release.tsv")
-independent_rna_primary_all <- rnaseq_samples %>%
-  independent_rna_samples(independent_dna_sample_df = independent_dna_sample_df_all,
-                          independent_level = "all-cohorts-pre-release",
-                          histology_df = .,
-                          match_type = "independent_dna_plus_only_rna",
-                          tumor_description_rna_only = "primary",
-                          seed = 2020) %>%
-  dplyr::arrange(Kids_First_Biospecimen_ID) %>% 
+# save output for all cohorts
+rnaseq_primary_all_file <- file.path(out_dir, "independent-specimens.rnaseq.primary-pre-release.tsv")
+message(paste(nrow(rnaseq_primary_all), "RNA-Seq primary specimens for all cohorts"))
+rnaseq_primary_all %>% 
   readr::write_tsv(rnaseq_primary_all_file)
 
-rnaseq_relapse_all_file <- file.path(out_dir, "independent-specimens.rnaseqpanel.relapse.pre-release.tsv")
-independent_rna_relapse_all <- rnaseq_samples %>%
-  independent_rna_samples(independent_dna_sample_df = independent_dna_sample_df_all,
-                          independent_level = "all-cohorts-pre-release",
-                          histology_df = .,
-                          match_type = "independent_dna_plus_only_rna",
-                          tumor_description_rna_only = "relapse",
-                          seed = 2020) %>%
-  dplyr::arrange(Kids_First_Biospecimen_ID) %>% 
+rnaseq_relapse_all_file <- file.path(out_dir, "independent-specimens.rnaseq.relapse-pre-release.tsv")
+message(paste(nrow(rnaseq_relapse_all), "RNA-Seq relapse specimens for all cohorts"))
+rnaseq_relapse_all %>% 
   readr::write_tsv(rnaseq_relapse_all_file)
 
-rnaseq_primplus_all_file <- file.path(out_dir, "independent-specimens.rnaseqpanel.primary-plus.pre-release.tsv")
-independent_rna_primary_plus_all <- rnaseq_samples %>%
-  independent_rna_samples(independent_dna_sample_df = independent_dna_sample_df_all,
-                          independent_level = "all-cohorts-pre-release",
-                          histology_df = .,
-                          match_type = "independent_dna_plus_only_rna",
-                          tumor_description_rna_only = "primary_plus",
-                          seed = 2020) %>%
-  dplyr::arrange(Kids_First_Biospecimen_ID) %>% 
+rnaseq_primplus_all_file <- file.path(out_dir, "independent-specimens.rnaseq.primary-plus-pre-release.tsv")
+message(paste(nrow(rnaseq_primary_plus_all), "RNA-Seq specimens (including non-primary) for all cohorts"))
+rnaseq_primary_plus_all %>% 
   readr::write_tsv(rnaseq_primplus_all_file)
