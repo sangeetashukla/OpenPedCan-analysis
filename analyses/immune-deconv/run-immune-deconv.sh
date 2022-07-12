df8fbd2f617946dc5e84122002931045fee5feab
kelseykeith <keithk@chop.edu>
2022-07-12 11:28:29 -0400

    commiting changes before final test run

--- analyses/immune-deconv/run-immune-deconv.sh
+++ analyses/immune-deconv/run-immune-deconv.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
-# Module author: Komal S. Rathi
-# 2022
+# Module author: Komal S. Rathi, updated Kelsey Keith
+# 2022-07
 
-# This script runs the steps for immune deconvolution using xCell and CIBERSORT (absolute mode). 
+# This script runs the steps for immune deconvolution using xCell and quanTIseq. 
 
 set -e
 set -o pipefail
-
-# Run original files - will not by default
-RUN_CIBERSORT=${RUN_CIBERSORT:-0}
 
 # This script should always run as if it were being called from
 # the directory it lives in.
@@ -17,9 +14,9 @@ script_directory="$(perl -e 'use File::Basename;
   print dirname(abs_path(@ARGV[0]));' -- "$0")"
 cd "$script_directory" || exit
 
-# xCell
+### xCell
 # generate deconvolution output
-echo "Deconvolution..."
+echo "Deconvolution xCell..."
 Rscript --vanilla 01-immune-deconv.R \
 --expr_mat '../../data/gene-expression-rsem-tpm-collapsed.rds' \
 --clin_file '../../data/histologies.tsv' \
@@ -27,28 +24,24 @@ Rscript --vanilla 01-immune-deconv.R \
 --output_dir 'results'
 
 # generate heatmaps of average normalized immune scores per cancer or gtex group
-echo "Create summary plots"
+echo "Create summary plots xCell"
 Rscript --vanilla 02-summary-plots.R \
 --deconv_output 'results/xcell_output.rds' \
 --output_dir 'plots'
 
-# CIBERSORT (absolute mode)
-if [ "$RUN_CIBERSORT" -gt "0" ]; then
+### quanTIseq
+# generate deconvolution output
+echo "Deconvolution quanTIseq..."
+Rscript --vanilla 01-immune-deconv.R \
+--expr_mat '../../data/gene-expression-rsem-tpm-collapsed.rds' \
+--clin_file '../../data/histologies.tsv' \
+--deconv_method 'quantiseq' \
+--output_dir 'results'
 
-	# generate deconvolution output
-	echo "Deconvolution..."
-	Rscript --vanilla 01-immune-deconv.R \
-	--expr_mat '../../data/gene-expression-rsem-tpm-collapsed.rds' \
-	--clin_file '../../data/histologies.tsv' \
-	--deconv_method 'cibersort_abs' \
-	--cibersort_mat 'util/LM22.txt' \
-	--cibersort_binary 'util/CIBERSORT.R' \
-	--output_dir 'results'
+# generate heatmaps of average normalized immune scores per cancer or gtex group
+echo "Create summary plots quanTIseq"
+Rscript --vanilla 02-summary-plots.R \
+--deconv_output 'results/quantiseq_output.rds' \
+--output_dir 'plots'
 
-	# generate heatmaps of average normalized immune scores per cancer or gtex group
-	echo "Create summary plots"
-	Rscript --vanilla 02-summary-plots.R \
-	--deconv_output 'results/cibersort_abs_output.rds' \
-	--output_dir 'plots'
 
-fi
