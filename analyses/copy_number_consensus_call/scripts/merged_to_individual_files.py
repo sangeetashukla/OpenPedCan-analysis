a6f97cc3141958fd6b53b5797b100eca510e3d44
Alex Sickler <sicklera@chop.edu>
2022-07-14 14:47:27 -0400

    :art: minor changes to adjust for actual gatk file

--- analyses/copy_number_consensus_call/scripts/merged_to_individual_files.py
+++ analyses/copy_number_consensus_call/scripts/merged_to_individual_files.py
@@ -18,7 +18,7 @@ extensions = {"gatk": ".gatk", "cnvkit": ".cnvkit", "freec": ".freec"}
 
 # Define the column headers for IDs
 id_headers = {
-    "gatk": "Kids_First_Biospecimen_ID",
+    "gatk": "BS_ID",
     "cnvkit": "ID",
     "freec": "Kids_First_Biospecimen_ID",
 }
