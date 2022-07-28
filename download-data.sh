452ba9192f6c1b863c221abe09ad97e3602a42d3
jharenza <jolynnerokita@d3b.center>
2022-07-27 21:17:37 -0400

    try removing /

--- download-data.sh
+++ download-data.sh
@@ -2,7 +2,7 @@
 set -e
 set -o pipefail
 
-# Use the OpenPBTA bucket as the default.
+# Use the OpenPedCan bucket as the default.
 URL=${OPENPEDCAN_URL:-https://s3.amazonaws.com/d3b-openaccess-us-east-1-prd-pbta/open-targets}
 RELEASE=${OPENPEDCAN_RELEASE:-v11}
 PREVIOUS=${OPENPEDCAN_RELEASE:-v10}
