9011f4962b8e8c7cb019a3e17246f689b2b33cb7
kelseykeith <keithk@chop.edu>
2022-08-19 10:13:53 -0400

    fixing read_tsv() error causing GitHub Actions to fail

--- analyses/snv-frequencies/01-snv-frequencies.R
+++ analyses/snv-frequencies/01-snv-frequencies.R
@@ -694,18 +694,21 @@ stopifnot(identical(
   as.integer(0)))
 
 # read in WGS/WXS MAF
-maf_df <- read_tsv(
-  '../../data/snv-consensus-plus-hotspots.maf.tsv.gz', comment = '#',
-  col_types = cols(
-    .default = col_guess(),
-    CLIN_SIG = col_character(),
-    PUBMED = col_character()))
+maf_df <- read_tsv('../../data/snv-consensus-plus-hotspots.maf.tsv.gz', 
+                   comment = '#',
+                   col_types = cols(.default = col_guess(),
+                                    CLIN_SIG = col_character(),
+                                    PUBMED = col_character(),
+                                    SOMATIC = col_character(),
+                                    PHENO = col_character()))
 # read in DGD MAF; has a slightly different format than the MAF above
 maf_df_dgd <- read_tsv('../../data/snv-dgd.maf.tsv.gz', 
                        comment = '#', 
                        col_types = cols(.default = col_guess(), 
                                         CLIN_SIG = col_character(), 
-                                        PUBMED = col_character()))
+                                        PUBMED = col_character(),
+                                        SOMATIC = col_character(),
+                                        PHENO = col_character()))
 # combine MAFs by binding rows since the DGD MAF is missing a couple columns
 maf_df <- bind_rows(maf_df, maf_df_dgd)
 # remove DGD MAF as it's no longer needed and doesn't need to be in memory
