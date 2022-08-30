d0885939bbee5d9bab6a2b2760810ee278db74b5
sangeetashukla <82055098+sangeetashukla@users.noreply.github.com>
2022-08-30 08:18:02 -0400

    Added gencode gtf files, updated scripts for processing

--- analyses/gene_match/merge_gencode_v28_v38_open_ped_can_v7_gene_ensg_symbol_mapping_files.R
+++ analyses/gene_match/merge_gencode_v28_v38_open_ped_can_v7_gene_ensg_symbol_mapping_files.R
@@ -1,12 +1,18 @@
 library(tidyverse)
+library(dplyr)
 
+v27_eh_df <- read_tsv('results/ensembl_gene_symbol_gtf_genode_v27.tsv')
 v28_eh_df <- read_tsv('results/ensembl_gene_symbol_gtf_genode_v28.tsv')
+v36_eh_df <- read_tsv('results/ensembl_gene_symbol_gtf_genode_v36.tsv')
 v38_eh_df <- read_tsv('results/ensembl_gene_symbol_gtf_genode_v38.tsv')
+v39_eh_df <- read_tsv('results/ensembl_gene_symbol_gtf_genode_v39.tsv')
 
 open_ped_can_v7_eh_df <- read_tsv('input/open_ped_can_v7_ensg-hugo-rmtl-mapping.tsv') %>%
-  select(gene_symbol, ensg_id) %>%
-  rename(ensembl = ensg_id)
+  dplyr::select(gene_symbol, ensg_id) %>%
+  dplyr::rename(ensembl = ensg_id)
 
-merged_eh_df <- distinct(bind_rows(v28_eh_df, v38_eh_df, open_ped_can_v7_eh_df))
+#merged_eh_df <- distinct(bind_rows(v28_eh_df, v38_eh_df, open_ped`_can_v7_eh_df))
+merged_eh_df <- distinct(bind_rows(v27_eh_df, v28_eh_df, v36_eh_df, v38_eh_df, v39_eh_df, open_ped_can_v7_eh_df))
 
-write_tsv(merged_eh_df, 'results/ensembl_gene_symbol_gtf_genode_v28_v38_open_ped_can_v7_merged.tsv')
+#write_tsv(merged_eh_df, 'results/ensembl_gene_symbol_gtf_genode_v28_v38_open_ped_can_v7_merged.tsv')
+write_tsv(merged_eh_df, 'results/ensembl_gene_symbol_gtf_genode_all_open_ped_can_v7_merged.tsv')
