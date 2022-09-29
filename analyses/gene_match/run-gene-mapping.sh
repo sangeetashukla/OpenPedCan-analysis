492cb29c7828c65e99c6b3075abd36136d764118
sangeetashukla <82055098+sangeetashukla@users.noreply.github.com>
2022-09-29 11:07:57 -0400

    Numbered scripts and updated filenames in scripts appropriately

--- analyses/gene_match/run-gene-mapping.sh
+++ analyses/gene_match/run-gene-mapping.sh
@@ -15,17 +15,17 @@ mkdir -p results
 
 ## Match ensembl to gene_symbol using gtf file
 
-Rscript --vanilla gene_ensembl_id_from_gtf.R --gtf_file input/gencode.v27.annotation.gtf.gz --output_file results/ensembl_gene_symbol_gtf_gencode_v27.tsv
-Rscript --vanilla gene_ensembl_id_from_gtf.R --gtf_file input/gencode.v28.annotation.gtf.gz --output_file results/ensembl_gene_symbol_gtf_gencode_v28.tsv
-Rscript --vanilla gene_ensembl_id_from_gtf.R --gtf_file input/gencode.v36.annotation.gtf.gz --output_file results/ensembl_gene_symbol_gtf_gencode_v36.tsv
-Rscript --vanilla gene_ensembl_id_from_gtf.R --gtf_file input/gencode.v39.annotation.gtf.gz --output_file results/ensembl_gene_symbol_gtf_gencode_v39.tsv
+Rscript --vanilla 01-gene-ensembl-id-from-gtf.R --gtf_file input/gencode.v27.annotation.gtf.gz --output_file results/ensembl_gene_symbol_gtf_gencode_v27.tsv
+Rscript --vanilla 01-gene-ensembl-id-from-gtf.R --gtf_file input/gencode.v28.annotation.gtf.gz --output_file results/ensembl_gene_symbol_gtf_gencode_v28.tsv
+Rscript --vanilla 01-gene-ensembl-id-from-gtf.R --gtf_file input/gencode.v36.annotation.gtf.gz --output_file results/ensembl_gene_symbol_gtf_gencode_v36.tsv
+Rscript --vanilla 01-gene-ensembl-id-from-gtf.R --gtf_file input/gencode.v39.annotation.gtf.gz --output_file results/ensembl_gene_symbol_gtf_gencode_v39.tsv
 
-## Merge gencode all results and input/open_ped_can_v7_ensg-hugo-rmtl-mapping.tsv
+## Merge all gencode results and input/open_ped_can_v7_ensg-hugo-rmtl-mapping.tsv
 
-Rscript --vanilla merge_gencode_ensg_symbol_map.R
+Rscript --vanilla 02-merge-gencode-ensg-symbol-map.R
 # creates gencode_ensg_symbol_map_merged.tsv
 
 ## Merge PMTL
-Rscript -e "rmarkdown::render('add_pmtl_ens_hugo.Rmd')"
+Rscript -e "rmarkdown::render('03-add-pmtl-ensg-hugo.Rmd')"
 
-Rscript -e "rmarkdown::render('qc_ensg_hugo_pmtl_mapping.Rmd', clean = TRUE)"
+Rscript -e "rmarkdown::render('04-qc-ensg-hugo-pmtl-mapping.Rmd', clean = TRUE)"
