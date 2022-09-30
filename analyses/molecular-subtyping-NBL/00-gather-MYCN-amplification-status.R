18818dab306acca026c82525f759f65f94bf1f48
adilahiri <lahiria@chop.edu>
2022-09-30 01:30:37 +0000

    Adding script

--- analyses/molecular-subtyping-NBL/00-gather-MYCN-amplification-status.R
+++ analyses/molecular-subtyping-NBL/00-gather-MYCN-amplification-status.R
@@ -1,5 +1,22 @@
 library(dplyr)
 library(data.table)
 
-# Load the Files in ticket
-consensus_wgs_plus_cnvkit_wxs_df<-as.data.frame(fread("/home/rstudio/OpenPedCan-analysis/data/consensus_wgs_plus_cnvkit_wxs.tsv.gz"))
+# Load the file consensus_wgs_plus_cnvkit_wxs.tsv.gz
+consensus_df<-as.data.frame(fread("/home/rstudio/OpenPedCan-analysis/data/consensus_wgs_plus_cnvkit_wxs.tsv.gz"))
+hist_df<- as.data.frame(fread("/home/rstudio/OpenPedCan-analysis/data/histologies.tsv"))
+
+# For each NBL sample, gather MYCN amplification status 
+
+## Step 1: Filter the neuroblastoma, ganglioneuroblastoma, and ganglioneuroma samples from hist file first
+hist_df <- hist_df %>% filter(pathology_diagnosis=="Neuroblastoma"|
+                                pathology_diagnosis=="Ganglioneuroblastoma"|
+                                pathology_diagnosis=="Ganglioneuroblastoma,nodular"|
+                                pathology_diagnosis=="Ganglioneuroblastoma, intermixed"|
+                                pathology_diagnosis=="Ganglioneuroma, maturing subtype OR Ganglioneuroblastoma, well differentiated")
+
+
+## Step 2: Filter the MCYN records from consensus_wgs_plus_cnvkit_wxs.tsv.gz
+consensus_df <- consensus_df %>% filter(gene_symbol=="MYCN")
+
+## Join the two data frames to get MYCN details with respect to the diagnosis
+Myc_df <- inner_join(hist_df,consensus_df,by= c("Kids_First_Biospecimen_ID" = "biospecimen_id"))
