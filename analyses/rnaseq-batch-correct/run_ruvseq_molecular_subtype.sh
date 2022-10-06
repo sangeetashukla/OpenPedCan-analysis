8bf730c47b124a2f4be0c2590318e5ae31f20bf3
aadamk <adamkraya@d3b.center>
2022-10-06 14:12:07 -0400

    update run scripts

--- analyses/rnaseq-batch-correct/run_ruvseq_molecular_subtype.sh
+++ analyses/rnaseq-batch-correct/run_ruvseq_molecular_subtype.sh
@@ -12,28 +12,14 @@ Rscript code/01-ruvseq-deseq.R \
 --dataset 'target_nbl' \
 --cohort_values 'TARGET' \
 --cancer_group_values 'Neuroblastoma' \
---k_value 5
-
-Rscript code/03-ruvseq-summarization.R \
---dataset 'target_nbl' \
---cohort_values 'TARGET' \
---cancer_group_values 'Neuroblastoma' \
---ruvg_dir 'deseq2_analysis' \
 --pos_c 'MYCN_targets_M2919_M18532.rds' \
 --neg_c 'hk_genes_normals.rds' \
---analysis_type 'tumor-only'
+--k_value 5
 
 Rscript code/01-ruvseq-deseq.R \
 --dataset 'hgg_dmg' \
 --cohort_values 'PBTA' \
 --cancer_group_values 'High-grade glioma/astrocytoma,Diffuse midline glioma' \
---k_value 5
-
-Rscript code/03-ruvseq-summarization.R \
---dataset 'hgg_dmg' \
---cohort_values 'PBTA' \
---cancer_group_values 'High-grade glioma/astrocytoma,Diffuse midline glioma' \
---ruvg_dir 'deseq2_analysis' \
 --pos_c '12915_2022_1324_MOESM4_ESM.rds' \
 --neg_c 'hk_genes_normals.rds' \
---analysis_type 'tumor-only'
+--k_value 5
--- analyses/rnaseq-batch-correct/run_ruvseq_tn.sh
+++ analyses/rnaseq-batch-correct/run_ruvseq_tn.sh
@@ -14,14 +14,6 @@ Rscript code/02-ruvseq-deseq-tn.R \
 --cancer_group_values 'Diffuse midline glioma,High-grade glioma/astrocytoma' \
 --gtex_subgroups 'Brain - Cortex,Brain - Cerebellum' \
 --k_value 5 \
---drop 2
-
-Rscript code/03-ruvseq-summarization.R \
---dataset 'HGG-DMG_TN' \
---cohort_values 'PBTA,GTEx' \
---cancer_group_values 'Diffuse midline glioma,High-grade glioma/astrocytoma' \
---ruvg_dir 'deseq2_analysis' \
+--drop 2 \
 --pos_c 'reactome_cell_cycle_msigdb_v7.5.1.rds' \
---neg_c 'hk_genes_normals.rds' \
---analysis_type 'tumor-normal' \
---gtex_subgroups 'Brain - Cortex,Brain - Cerebellum'
+--neg_c 'hk_genes_normals.rds'
