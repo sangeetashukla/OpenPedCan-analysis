e664c02062870880798c2719e04f631d673ba002
aadamk <adamkraya@d3b.center>
2022-10-18 13:44:58 -0400

    remove archive and fix filenames

--- analyses/rnaseq-batch-correct/code/archive/run_pca_umap_clustering.sh
+++
@@ -1,8 +0,0 @@
-# compute PCA and UMAP from count data
-Rscript 01-full_dataset_compute_pca_umap_counts.R
-
-# create PCA plots 
-Rscript 02-full_dataset_plot_pca_counts.R
-
-# create UMAP plots
-Rscript 02-full_dataset_plot_umap_counts.R
