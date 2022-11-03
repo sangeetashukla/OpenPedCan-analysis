0e5ec2e0d176e689fce348e69ccfd1d0505aae99
Dave Hill <dhill4213@gmail.com>
2022-11-03 14:42:52 -0400

    Update analyses/molecular-subtyping-EPN/02_ependymoma_generate_all_data.R
    Co-authored-by: Ryan Corbett <corbettr@chop.edu>

--- analyses/molecular-subtyping-EPN/02_ependymoma_generate_all_data.R
+++ analyses/molecular-subtyping-EPN/02_ependymoma_generate_all_data.R
@@ -238,7 +238,7 @@ EPN_notebook[,"H3F3A_HGVSp_Short"] <- unlist(sapply(EPN_notebook$Kids_First_Bios
 mutation_gene <- mutations %>% filter(Hugo_Symbol == "H3F3B" & HGVSp_Short == 'p.K28M')
 EPN_notebook[,"H3F3B_HGVSp_Short"] <- unlist(sapply(EPN_notebook$Kids_First_Biospecimen_ID_DNA,
                                                     function(x) mutation_fill_df(sample = x, ref_df = mutation_gene, col_name = 'HGVSp_Short')))
-mutation_gene <- mutations %>% filter(Hugo_Symbol == "HIST1H3B")
+mutation_gene <- mutations %>% filter(Hugo_Symbol == "HIST1H3B" & HGVSp_Short == 'p.K28M')
 EPN_notebook[,"HIST1H3B_HGVSp_Short"] <- unlist(sapply(EPN_notebook$Kids_First_Biospecimen_ID_DNA,
                                                     function(x) mutation_fill_df(sample = x, ref_df = mutation_gene, col_name = 'HGVSp_Short')))
 mutation_gene <- mutations %>% filter(Hugo_Symbol == "HIST1H3C")
