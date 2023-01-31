e407b278ff63c580b50da7ff92b18f9542be9e5d
zzgeng <zz.geng@hotmail.com>
2023-01-31 15:18:58 +0000

    change archive folder name and update bash script

--- analyses/molecular-subtyping-ATRT/run-molecular-subtyping-ATRT.sh
+++ analyses/molecular-subtyping-ATRT/run-molecular-subtyping-ATRT.sh
@@ -6,7 +6,7 @@ set -o pipefail
 # set up running directory
 cd "$(dirname "${BASH_SOURCE[0]}")" 
 
-# Run R script to subtype ATRT 
+# Run R script to subtype ATRT using methylation data 
 
 Rscript 00-ATRT_subtyping.R
 
