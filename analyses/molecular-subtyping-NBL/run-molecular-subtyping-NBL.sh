1f9351799e2d1c9e1d3a8f440d46c1f6cade92cf
jharenza <jharenza@gmail.com>
2023-02-01 21:43:10 -0500

    separate subset script to use terms and renumber scripts
    - also condensed code in script 01-subset-for-NBL

--- analyses/molecular-subtyping-NBL/run-molecular-subtyping-NBL.sh
+++ analyses/molecular-subtyping-NBL/run-molecular-subtyping-NBL.sh
@@ -7,16 +7,17 @@ set -o pipefail
 cd "$(dirname "${BASH_SOURCE[0]}")" 
 
 # Run notebook to subtype MYCN NBL 
+Rscript 00-select-diagnoses.R
 
-Rscript -e "rmarkdown::render('00-subset-for-NBL.Rmd')"
+Rscript -e "rmarkdown::render('01-subset-for-NBL.Rmd')"
 
-Rscript -e "rmarkdown::render('01-find-matched-biospecimen.Rmd')"
+Rscript -e "rmarkdown::render('02-find-matched-biospecimen.Rmd')"
 
-Rscript -e "rmarkdown::render('02-find-non-matching-biospecimen.Rmd')"
+Rscript -e "rmarkdown::render('03-find-non-matching-biospecimen.Rmd')"
 
-Rscript -e "rmarkdown::render('03-subtyping.Rmd')"
+Rscript -e "rmarkdown::render('04-subtyping.Rmd')"
 
-Rscript -e "rmarkdown::render('04-qc-checks.Rmd')"
+Rscript -e "rmarkdown::render('05-qc-checks.Rmd')"
 
 echo 'NBL Subtyping Completed'
 
