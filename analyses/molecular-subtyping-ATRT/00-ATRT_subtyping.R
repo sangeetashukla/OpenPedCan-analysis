496d5be5c9abf4e551315582b965924a7acea71d
jharenza <jharenza@gmail.com>
2023-02-03 21:05:05 -0500

    adding DGD panel samples to this

--- analyses/molecular-subtyping-ATRT/00-ATRT_subtyping.R
+++ analyses/molecular-subtyping-ATRT/00-ATRT_subtyping.R
@@ -22,27 +22,43 @@ histo <-
 # select all ATRT biospecimens from PBTA and/DGD
 atrt_df <- histo %>%
   dplyr::filter(short_histology == "ATRT",
-                sample_type == "Tumor",
                 cohort == "PBTA" | cohort == "DGD")
 
 # Create a dataframe with sample_id and matched biospecimens id for RNA-Seq, WGS and methylation
-atrt_df_meth <- atrt_df %>% 
+atrt_df_methyl <- atrt_df %>% 
   filter(experimental_strategy == "Methylation") %>%
   select(sample_id, Kids_First_Biospecimen_ID, 
-         cns_methylation_subclass, cns_methylation_subclass_score)
+         cns_methylation_subclass, cns_methylation_subclass_score) %>%
+  dplyr::rename(Kids_First_Biospecimen_ID_methyl = Kids_First_Biospecimen_ID)
+
+atrt_df_panel_dna <- atrt_df %>% 
+  filter(experimental_strategy == "Targeted Sequencing",
+         is.na(RNA_library)) %>%
+  select(sample_id,  Kids_First_Biospecimen_ID) %>%
+  dplyr::rename(Kids_First_Biospecimen_ID_DNA_panel = Kids_First_Biospecimen_ID)
+
+atrt_df_panel_rna <- atrt_df %>% 
+  filter(experimental_strategy == "Targeted Sequencing",
+         !is.na(RNA_library)) %>%
+  select(sample_id,  Kids_First_Biospecimen_ID) %>%
+  dplyr::rename(Kids_First_Biospecimen_ID_RNA_panel = Kids_First_Biospecimen_ID)
 
 atrt_df_WGS <- atrt_df %>% 
   filter(experimental_strategy == "WGS") %>%
-  select(sample_id,  Kids_First_Biospecimen_ID)
+  select(sample_id,  Kids_First_Biospecimen_ID) %>%
+  dplyr::rename(Kids_First_Biospecimen_DNA_wgs = Kids_First_Biospecimen_ID)
 
 atrt_df_RNA <- atrt_df %>% 
   filter(experimental_strategy == "RNA-Seq") %>%
-  select(sample_id,  Kids_First_Biospecimen_ID) 
+  select(sample_id,  Kids_First_Biospecimen_ID) %>%
+  dplyr::rename(Kids_First_Biospecimen_ID_RNA = Kids_First_Biospecimen_ID)
 
-atrt_subtype <- atrt_df_meth %>%
-  full_join(atrt_df_WGS, by = "sample_id", suffix = c("_meth", "_DNA")) %>% 
-  full_join(atrt_df_RNA, by = "sample_id") %>% 
-  rename(Kids_First_Biospecimen_ID_RNA = Kids_First_Biospecimen_ID)
+atrt_subtype <- atrt_df_methyl %>%
+  full_join(atrt_df_WGS, by = "sample_id") %>% 
+  full_join(atrt_df_RNA, by = "sample_id") %>%
+  full_join(atrt_df_panel_dna, by = "sample_id") %>%
+  full_join(atrt_df_panel_rna, by = "sample_id")
+  
 
 # create a list for ATRT subtypes
 ATRT_subtype_list <- c("ATRT, MYC", "ATRT, SHH", "ATRT, TYR")
@@ -50,11 +66,11 @@ ATRT_subtype_list <- c("ATRT, MYC", "ATRT, SHH", "ATRT, TYR")
 # for the samples, whose cns_methlation_subclass_score >= 0.8 and cns_methylation_subclass is one of the three types in ATRT_subtype_list, their molecular subtype are same as cns_methylation_subclass
 # for the samples without methylation sequencing, their molecular subtype are "ATRT, To be classified."
 # For the samples fit all the other situations, their molecular subtype are "ATRT, To be classified."
-atrt_subtype <- atrt_subtype %>% 
+atrt_subtype_final <- atrt_subtype %>% 
   mutate(molecular_subtype = case_when(cns_methylation_subclass_score >= 0.8 & cns_methylation_subclass %in% ATRT_subtype_list ~ cns_methylation_subclass, 
-                                       is.na(Kids_First_Biospecimen_ID_meth) ~ "ATRT, To be classified",
+                                       is.na(Kids_First_Biospecimen_ID_methyl) ~ "ATRT, To be classified",
                                        TRUE ~ "ATRT, To be classified")) %>%
-  select(sample_id, Kids_First_Biospecimen_ID_meth, Kids_First_Biospecimen_ID_DNA, Kids_First_Biospecimen_ID_RNA, molecular_subtype) %>%
+  select(sample_id, Kids_First_Biospecimen_ID_methyl, Kids_First_Biospecimen_DNA_wgs, Kids_First_Biospecimen_ID_RNA, Kids_First_Biospecimen_ID_DNA_panel, Kids_First_Biospecimen_ID_RNA_panel, molecular_subtype) %>%
   
 # write result
   readr::write_tsv(file.path(results_dir, "ATRT-molecular-subtypes.tsv"))
