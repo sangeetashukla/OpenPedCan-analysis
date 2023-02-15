31ef1a0b2c50f4e1cde0023cea097cd7f7bef3aa
Eric Wafula <ewafula@gmail.com>
2023-02-14 19:07:30 -0500

    review updates

--- analyses/mtp-annotations/run-mtp-annotations.sh
+++ analyses/mtp-annotations/run-mtp-annotations.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Dave Hill, OPenPedCan 2023
+# Dave Hill and Eric Wafula OPenPedCan 2023
 # Run OpenPedCan modules to generate MTP tables
 
 set -e
@@ -7,57 +7,43 @@ set -o pipefail
 
 printf "Start generating mtp tables...\n\n"
 
-#Set the desired folder locations for each file type
-csv_diseases_folder="/home/ubuntu/volume/OpenPedCan-analysis/scratch/mtp-csv/diseases"
-csv_targets_folder="/home/ubuntu/volume/OpenPedCan-analysis/scratch/mtp-csv/targets"
-json_diseases_folder="/home/ubuntu/volume/OpenPedCan-analysis/scratch/mtp-json/diseases"
-json_targets_folder="/home/ubuntu/volume/OpenPedCan-analysis/scratch/mtp-json/targets"
+# Set MTP Open Targets FTP download links for diseases and targets
+version="22.11"
+diseases_url="ftp://ftp.ebi.ac.uk/pub/databases/opentargets/platform/$version/output/etl/json/diseases"
+targets_url="ftp://ftp.ebi.ac.uk/pub/databases/opentargets/platform/$version/output/etl/json/targets"
 
 
-#Recursively download all of the diseases json files into the mtp-json/diseases scratch folder
-wget -P /home/ubuntu/volume/OpenPedCan-analysis/scratch/mtp-json/ --recursive --no-parent --no-host-directories --cut-dirs 8 ftp://ftp.ebi.ac.uk/pub/databases/opentargets/platform/22.11/output/etl/json/diseases
+# This script should always run as if it were being called from the directory it lives in.
+module_dir="$(perl -e 'use File::Basename;
+  use Cwd "abs_path";
+  print dirname(abs_path(@ARGV[0]));' -- "$0")"
+cd "$module_dir" || exit
 
+# Set sratch directory for dowloads and temporary files
+scratch_dir="$module_dir/../../scratch"
 
-#Recursively download all of the targets json files into the mtp-json/targets scratch folder
-wget -P /home/ubuntu/volume/OpenPedCan-analysis/scratch/mtp-json/ --recursive --no-parent --no-host-directories --cut-dirs 8 ftp://ftp.ebi.ac.uk/pub/databases/opentargets/platform/22.11/output/etl/json/targets
+# Set directories to download diseases and targets json files
+json_diseases_dir="$scratch_dir/mtp-json"
+mkdir -p $json_diseases_dir
+json_targets_dir="$scratch_dir/mtp-json"
+mkdir -p $json_targets_dir
 
-#Move to the mtp-json/diseases folder
-cd $json_diseases_folder
+# Set directories to convert diseases and targets json files json to csv format
+export csv_diseases_dir="$scratch_dir/mtp-csv/diseases"
+mkdir -p $csv_diseases_dir
+export csv_targets_dir="$scratch_dir/mtp-csv/targets"
+mkdir -p $csv_targets_dir
 
-#Make an array of the names of each json file in the mtp-json/diseases folder
-dirlist=(`ls ${prefix}*.json`)
+# Recursively download diseases and targets json files
+wget -P $json_diseases_dir --recursive --no-parent --no-host-directories --cut-dirs 8 $diseases_url
+wget -P $json_targets_dir --recursive --no-parent --no-host-directories --cut-dirs 8 $targets_url
 
+# Convert diseases and targets json files to csv format
+cd $json_diseases_dir/diseases
+perl -e 'foreach (<*.json>){/(.*)\.json$/; $name = $1; system "dasel -r json -w csv < $_ > $ENV{csv_diseases_dir}/$name.csv";}'
+cd $json_targets_dir/targets
+perl -e 'foreach (<*.json>){/(.*)\.json$/; $name = $1; system "dasel -r json -w csv < $_ > $ENV{csv_targets_dir}/$name.csv";}'
 
-#Runs a loop to change each mtp-json/diseases/ file into a csv file
-for filelist in $json_diseases_folder/*.json
-do 
-	echo "$(basename  $filelist .json)"
-	name=$(basename  $filelist .json)
-	echo "$filelist"
-	dasel -r json -w csv < $filelist > $name.csv
-
-done
-
-#Move all csv files in the mtp-json/diseaes folder to the mtp-csv/diseases folder
-mv $json_diseases_folder/*.csv $csv_diseases_folder/.
-
-#Move to the mtp-json/targets folder
-cd $json_targets_folder
-
-##Make an array of the names of each json file in the mtp-json/targets folder
-dirlist=(`ls ${prefix}*.json`)
-
-#Runs a loop to change each mtp-json/targets/ file into a csv file
-for filelist in $json_targets_folder/*.json
-do
-        echo "$(basename  $filelist .json)"
-        name=$(basename  $filelist .json)
-        echo "$filelist"
-        dasel -r json -w csv < $filelist > $name.csv
-
-done
-
-#Move all csv files in the mtp-json/targets folder to the mtp-csv/targets folder
-mv $json_targets_folder/*.csv $csv_targets_folder/.
-
-Rscript ~/volume/OpenPedCan-analysis/analyses/mtp-annotations/01-mtp-annotations.R 
+# Create diseases and targets mapping files
+cd $module_dir
+Rscript --vanilla 01-mtp-annotations.R
